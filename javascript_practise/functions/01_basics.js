3ad23d79145f6d051e1b046573c7b8d8a47ff6ad
shivamsinghAIMLops32 <shivamsinghcoder92@gmail.com>
2023-11-26 09:35:33 +0530

    JS file

---
+++ javascript_practise/functions/01_basics.js
@@ -0,0 +1,16 @@
+// basic function
+
+function helloWorld(){
+  return helloWorld;
+};
+
+console.log(helloWorld());
+
+// ADD two numbers x and y and Return sum
+
+function addNum(num1,num2){
+  return num1+num2;
+};
+
+console.log(addNum(5,9));
+console.log(typeof addNum());              // number as output
